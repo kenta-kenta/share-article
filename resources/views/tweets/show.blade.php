36f37c592ecd5bb26d8297e8e996180c8cc49b84
kenta-kenta <kenken22352235@gmail.com>
2024-10-04 18:00:20 +0900

    リンクの下線をつけた

--- resources/views/tweets/show.blade.php
+++ resources/views/tweets/show.blade.php
@@ -10,58 +10,14 @@
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        @if ($tweet->is_story)
         <div class="p-6 text-gray-900 dark:text-gray-100">
           <a href="{{ route('tweets.index') }}" class="text-blue-500 hover:text-blue-700 mr-2">一覧に戻る</a>
+          @if ($tweet->is_story)
           <p class="text-green-500">特別投稿</p>
-          <a href="{{ $tweet->article_url }}" class="text-gray-400 hover:text-gray-500 text-lg inline-block">{{ $tweet->article }}</a>
-          <p class="text-gray-800 dark:text-gray-300 text-lg">{{ $tweet->tweet }}</p>
-          <p class="text-gray-600 dark:text-gray-400 text-sm">投稿者: {{ $tweet->user->name }}</p>
-          <div class="text-gray-600 dark:text-gray-400 text-sm">
-            <p>作成日時: {{ $tweet->created_at->format('Y-m-d H:i') }}</p>
-            <p>更新日時: {{ $tweet->updated_at->format('Y-m-d H:i') }}</p>
-          </div>
-          @if (auth()->id() == $tweet->user_id)
-          <div class="flex mt-4">
-            <a href="{{ route('tweets.edit', $tweet) }}" class="text-blue-500 hover:text-blue-700 mr-2">編集</a>
-            <form action="{{ route('tweets.destroy', $tweet) }}" method="POST" onsubmit="return confirm('本当に削除しますか？');">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="text-red-500 hover:text-red-700">削除</button>
-            </form>
-          </div>
-          @endif
-          <div class="flex mt-4">
-            @if ($tweet->liked->contains(auth()->id()))
-            <form action="{{ route('tweets.dislike', $tweet) }}" method="POST">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="text-red-500 hover:text-red-700">dislike {{$tweet->liked->count()}}</button>
-            </form>
-            @else
-            <form action="{{ route('tweets.like', $tweet) }}" method="POST">
-              @csrf
-              <button type="submit" class="text-blue-500 hover:text-blue-700">like {{$tweet->liked->count()}}</button>
-            </form>
-            @endif
-          </div>
-          <div class="mt-4">
-            <p class="text-gray-600 dark:text-gray-400">comment {{ $tweet->comments->count() }}</p>
-            <a href="{{ route('tweets.comments.create', $tweet) }}" class="text-blue-500 hover:text-blue-700 mr-2">コメントする</a>
-          </div>
-          <div class="mt-4">
-            @foreach ($tweet->comments as $comment)
-            <a href="{{ route('tweets.comments.show', [$tweet, $comment]) }}">
-              <p>{{ $comment->comment }} <span class="text-gray-600 dark:text-gray-400 text-sm">{{ $comment->user->name }} {{ $comment->created_at->format('Y-m-d H:i') }}</span></p>
-            </a>
-            @endforeach
-          </div>
-        </div>
-        @else
-        <div class="p-6 text-gray-900 dark:text-gray-100">
-          <a href="{{ route('tweets.index') }}" class="text-blue-500 hover:text-blue-700 mr-2">一覧に戻る</a>
+          @else
           <p class="text-blue-500">通常投稿</p>
-          <a href="{{ $tweet->article_url }}" class="text-gray-400 hover:text-gray-500 text-lg inline-block">{{ $tweet->article }}</a>
+          @endif
+          <a href="{{ $tweet->article_url }}" class="text-gray-400 hover:text-gray-500 text-lg inline-block underline">{{ $tweet->article }}</a>
           <p class="text-gray-800 dark:text-gray-300 text-lg">{{ $tweet->tweet }}</p>
           <p class="text-gray-600 dark:text-gray-400 text-sm">投稿者: {{ $tweet->user->name }}</p>
           <div class="text-gray-600 dark:text-gray-400 text-sm">
@@ -104,7 +60,6 @@
             @endforeach
           </div>
         </div>
-        @endif
       </div>
     </div>
   </div>
